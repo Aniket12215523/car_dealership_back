fd335aff158af344e2fd921087b715222937323a
Aniket12215523 <115388792+Aniket12215523@users.noreply.github.com>
2025-06-27 16:35:43 +0530

    updated backend with login register

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 
 import carRoutes from './routes/cars.js';
 import bookingRoutes from './routes/booking.js';
+import authRoutes from './routes/authRoutes.js';
 
 
 dotenv.config();
@@ -18,10 +19,11 @@ app.use(cors({
 
 
 app.use('/api/cars', carRoutes);
-app.use('/api/bookings', bookingRoutes);  
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/auth', authRoutes);  
 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI,)
 .then(() => {
   console.log('MongoDB connected');
 
