c880dff66be3116433e9b3cea7bed9543809076e
Aniket12215523 <115388792+Aniket12215523@users.noreply.github.com>
2025-07-09 14:35:08 +0530

    express 4.18.2 version

--- routes/cars.js
+++ routes/cars.js
@@ -5,29 +5,14 @@ const router = express.Router();
 
 
 router.get('/', async (req, res) => {
-  try {
-    const cars = await Car.find();
-    res.status(200).json(cars);
-  } catch (error) {
-    console.error('Error fetching cars:', error);
-    res.status(500).json({ message: 'Failed to fetch cars', error: error.message });
-  }
+  const cars = await Car.find();
+  res.json(cars);
 });
 
 
 router.get('/:id', async (req, res) => {
-  try {
-    const car = await Car.findById(req.params.id);
-
-    if (!car) {
-      return res.status(404).json({ message: 'Car not found' });
-    }
-
-    res.status(200).json(car);
-  } catch (error) {
-    console.error('Error fetching car by ID:', error);
-    res.status(500).json({ message: 'Failed to fetch car', error: error.message });
-  }
+  const car = await Car.findById(req.params.id);
+  res.json(car);
 });
 
 export default router;
--- server.js
+++ server.js
@@ -4,40 +4,38 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 
+
 import carRoutes from './routes/cars.js';
 import bookingRoutes from './routes/booking.js';
 import authRoutes from './routes/authRoutes.js';
 import profileRoutes from './routes/profileRoutes.js';
 import paymentRoutes from './routes/paymentRoutes.js';
 
+
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT ;
 
 app.use(cors({
   origin: ['http://localhost:5173', 'https://car-dealership-front.vercel.app'],
-  credentials: true,
+  credentials: true
 }));
-
-
-app.options('*', cors());
-
 
 app.use(express.json());
 
 
 app.use('/api/cars', carRoutes);
 app.use('/api/bookings', bookingRoutes);
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes); 
 app.use('/api/profile', profileRoutes);
 app.use('/api/payment', paymentRoutes);
-app.use('/uploads', express.static(path.join(process.cwd(), 'public', 'uploads')));
+app.use('/uploads', express.static(path.join(process.cwd(), 'public', 'uploads'))); 
 
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected');
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error(err));
+mongoose.connect(process.env.MONGO_URI,)
+.then(() => {
+  console.log('MongoDB connected');
+
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch((err) => console.error(err));
